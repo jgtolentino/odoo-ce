4564f07d8a778b6630eb85b3d0c9348f275f1c0b
jgtolentino <jgtolentino_rn@yahoo.com>
2025-11-24 10:53:57 +0800

    Add finance person directory seed data

--- addons/ipai_finance_ppm/__manifest__.py
+++ addons/ipai_finance_ppm/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "IPAI Finance PPM",
     "summary": "Finance Project Portfolio Management (Notion Parity).",
-    "version": "18.0.1.0.2",
+    "version": "18.0.1.0.3",
     "category": "Accounting/Finance",
     "author": "InsightPulseAI",
     "website": "https://insightpulseai.net",
@@ -14,6 +14,7 @@
     ],
     "data": [
         "security/ir.model.access.csv",
+        "data/finance_person_directory.xml",
         "views/finance_person_views.xml",
         "views/finance_task_views.xml",
         "views/bir_schedule_views.xml",
