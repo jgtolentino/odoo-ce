b665faeddeb398104a951647dd292e47e826c90a
jgtolentino <jgtolentino_rn@yahoo.com>
2025-11-24 10:26:44 +0800

    Remove binary module icons

--- addons/ipai_finance_ppm/__manifest__.py
+++ addons/ipai_finance_ppm/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "IPAI Finance PPM",
     "summary": "Finance Project Portfolio Management (Notion Parity).",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.2",
     "category": "Accounting/Finance",
     "author": "InsightPulseAI",
     "website": "https://insightpulseai.net",
