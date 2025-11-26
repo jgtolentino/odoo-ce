928a453d78aeae6707db5f3bd582fcbeae047d71
Jake Tolentino <jgtolentino_rn@yahoo.com>
2025-11-26 14:49:51 +0800

    fix: Add missing model imports to ipai_finance_ppm
    - Added finance_ppm import (was missing)
    - Added project_task import (was missing)
    - Ensures all models load correctly on module installation

--- addons/ipai_finance_ppm/models/__init__.py
+++ addons/ipai_finance_ppm/models/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from . import finance_ppm
 from . import finance_person
 from . import finance_task
 from . import bir_schedule
+from . import project_task
 from . import ppm_dashboard
